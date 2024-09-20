7e367d9e889bac856e722cc1910838c9bb64f3a0
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-09-20 05:11:17 +0200

    flying/free move cube chunk

--- ComputerInfo/build.bat
+++ ComputerInfo/build.bat
@@ -21,11 +21,6 @@ REM Use /showIncludes for include debugging
 
 REM Create main program
 cl ^
-    /I "C:/Program Files (x86)/Windows Kits/10/Include/10.0.26100.0/um" ^
-    /I "C:/Program Files (x86)/Windows Kits/10/Include/10.0.26100.0/ucrt" ^
-    /I "C:/Program Files (x86)/Windows Kits/10/Include/10.0.26100.0/cppwinrt" ^
-    /I "C:/Program Files (x86)/Windows Kits/10/Include/10.0.26100.0/shared" ^
-    /I "C:/Program Files (x86)/Windows Kits/10/Include/10.0.26100.0/winrt" ^
     /MT /nologo /Gm- /GR- /EHsc /Od /Oi /WX /W4 /FC /Z7 /wd4201 ^
     /fp:precise /Zc:wchar_t /Zc:forScope /Zc:inline /std:c++20 ^
     /D WIN32 /D _WINDOWS /D _UNICODE /D UNICODE /D _CRT_SECURE_NO_WARNINGS ^
@@ -33,9 +28,6 @@ cl ^
     "info_win32.cpp"^
     /link /INCREMENTAL:no ^
     /SUBSYSTEM:CONSOLE /MACHINE:X64 ^
-    /LIBPATH:"C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.39.33519/lib/x64" ^
-    /LIBPATH:"C:/Program Files (x86)/Windows Kits/10/Lib/10.0.26100.0/um/x64" ^
-    /LIBPATH:"C:/Program Files (x86)/Windows Kits/10/Lib/10.0.26100.0/ucrt/x64" ^
     kernel32.lib user32.lib gdi32.lib winmm.lib ^
     Advapi32.lib wbemuuid.lib iphlpapi.lib ws2_32.lib ^
     d3d12.lib dxgi.lib
